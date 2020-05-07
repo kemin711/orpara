1d3f61c96435913b77373f11547b71300e265009
Kemin Zhou <kmzhou4@yahoo.com>
2020-05-07 12:54:31 -0700

    update kmerbase

--- src/seqaln/kmerbase.h
+++ src/seqaln/kmerbase.h
@@ -53,15 +53,20 @@ template <int K> class KmerBase {
          return v;
       }
 
-      static uint64_t* chop(const string& seq) {
+      /**
+       * This method should not be used for performance.
+       * Instead copy paste this piece of code for particular 
+       * situations.
+       */
+      static uint64_t* kmerize(const string& seq) {
          uint64_t* res = new uint64_t[seq.size()-K+1];
          int i;
          uint64_t v=0;
-         for (i=0; i<K-1; ++i) { // first kmer except the last base
+         for (i=0; i<K; ++i) { // first kmer except the last base
             v <<= 2;
             v |= base2int(seq[i]);
          }
-         for (i=0; i < seq.length()-K+1; ++i) { // rolling update
+         for (i=0; i < seq.length()-K; ++i) { // rolling update
             v<<=2;
             v |= base2int(seq[i+K]);
             v &= mask;
@@ -101,7 +106,7 @@ template <int K> class KmerBase {
       static uint64_t getLeftMask() { return maskL; }
       static uint64_t getRightMask() { return maskR; }
 
-   protected:
+   public:
       /** 
        * mask is pow(4,K)-1 
        * 1000...0 63 0's
@@ -110,25 +115,25 @@ template <int K> class KmerBase {
        * K=3  --6---
        * 00000111111
        */
-      static uint64_t mask;
+      static const uint64_t mask;
       /**
        * K-1 left word from kmer
        * For K=3
        * 111100
        */
-      static uint64_t maskL;
+      static const uint64_t maskL;
       /**
        * K-1 right word from kmer
        * For K=3
        * 001111
        */
-      static uint64_t maskR;
+      static const uint64_t maskR;
 };
 
 template<int K>
-uint64_t KmerBase<K>::mask = ((~uint64_t(0))<<(64-2*K))>>(64-2*K);
-template<int K> uint64_t KmerBase<K>::maskL=KmerBase<K>::mask << 2;
-template<int K> uint64_t KmerBase<K>::maskR=KmerBase<K>::mask >> 2;
+const uint64_t KmerBase<K>::mask = ((~uint64_t(0))<<(64-2*K))>>(64-2*K);
+template<int K> const uint64_t KmerBase<K>::maskL=KmerBase<K>::mask << 2;
+template<int K> const uint64_t KmerBase<K>::maskR=KmerBase<K>::mask >> 2;
 
 
 #endif
--- src/seqaln/test/kmer_test.cpp
+++ src/seqaln/test/kmer_test.cpp
@@ -32,6 +32,20 @@ TEST_F(KmerTest, common) {
    uint64_t msk = KmerBase<3>::getMask();
    uint64_t l_msk = KmerBase<3>::getLeftMask();
    uint64_t r_msk = KmerBase<3>::getRightMask();
-   cout << std::hex << msk << " " << l_msk << " " << r_msk << endl;
+   cout << "mask: " << std::hex << msk << " " << l_msk << " " << r_msk << endl;
+
+   uint64_t msk31 = KmerBase<31>::getMask();
+   uint64_t l_msk31 = KmerBase<31>::getLeftMask();
+   uint64_t r_msk31 = KmerBase<31>::getRightMask();
+   cout << "31 bit mask: " << std::hex << msk31 << " " << l_msk31 << " " << r_msk31 << endl;
+
+   uint64_t* kint = KmerBase<31>::kmerize(rawseq);
+   size_t numKmer = rawseq.size()-30;
+   for (size_t i=0; i<numKmer; ++i) {
+      cout << std::dec << kint[i] << " ";
+   }
+   cout << endl;
+   delete kint;
+
    ASSERT_TRUE(msk == 63);
 }
