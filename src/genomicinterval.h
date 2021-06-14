3b7f020f2bf88ba16836e5659ad10d77737c0b26
Kemin Zhou <kmzhou4@yahoo.com>
2021-06-14 09:54:50 -0700

    add missing header

---
+++ src/genomicinterval.h
@@ -0,0 +1,28 @@
+#ifndef GENOMICINTERVAL_H
+#define GENOMICINTERVAL_H
+
+#include "interval.h"
+// expands the interval with reference id
+
+namespace orpara {
+/**
+ * A simple representation of a region on one genomic DNA.
+ */
+class GenomicInterval : public Interval {
+   GenomicInterval(int ri, int b, int e) 
+      : Interval(b,e), refid(ri)
+   {  }
+   bool operator<(const GenomicInterval& other) const;
+   bool operator>(const GenomicInterval& other) const;
+   bool operator==(const GenomicInterval& other) const;
+
+   protected:
+      /**
+       * Integer representation of the reference sequence
+       * the caller should know how to interpret the id int
+       * meaningful names, such as chr1, chrM, chrX, or chrY
+       */
+      int refid;
+};
+}
+#endif
