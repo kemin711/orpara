30cb7268195b5677b26c5d7c50e233d5b75b1478
Kemin Zhou <kmzhou4@yahoo.com>
2021-07-27 17:59:35 -0700

    add new file static member initialization

---
+++ src/seqaln/kmerbase.cpp
@@ -0,0 +1,6 @@
+#include "kmerbase.h"
+
+const char Nucleotide::int2base[4]={'A', 'C', 'G', 'T'};
+const char Nucleotide::int2baseC[4]={'C', 'A', 'G', 'T'};
+const char Nucleotide::int2baseG[4]={'G', 'A', 'C', 'T'};
+const char Nucleotide::int2baseT[4]={'T', 'A', 'C', 'G'};
